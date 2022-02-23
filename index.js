bb8dd99e1ab03bbbb91d8bb87a87a671456ec94b
unknown <lilliantyiso@gmail.com>
2022-02-23 12:17:14 +0200

    added settimeout

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const randomMathElem = document.querySelector('.randomMath');
 const textElem = document.querySelector('.text');
-const randomNumber = Math.ceil((Math.random() * 100));
+let randomNumber = Math.ceil((Math.random() * 100));
 
 
 
@@ -8,15 +8,22 @@ const randomNumber = Math.ceil((Math.random() * 100));
 
 function guessingGame() {
     if (randomMathElem.value > randomNumber) {
-        textElem.innerHTML = 'Your guess is too high';
-    }
+        textElem.innerHTML = 'Your guess is too high &#128576';
+    } else if (randomMathElem.value < randomNumber) {
+        textElem.innerHTML = 'Your guess is too low &#128575';
+    } else if (randomMathElem.value == randomNumber) {
+        textElem.innerHTML = `Correct, the secret number is ${randomNumber}  &#128513`;
+        setTimeout(function() {
+            textElem.innerHTML = 'New game started! '
+            randomNumber = Math.ceil((Math.random() * 100));
+        }, 3000)
+    } //else {
+    // randomMathElem.value && randomNumber != ''
+    // }
 
-    else if (randomMathElem.value < randomNumber) {
-        textElem.innerHTML = 'Your guess is too low';
-    }
+    //randomMathElem.value = ''
+    // setTimeout(function() {
+    //     textElem.innerHTML = ''
+    // }, 4000)
 
-    else {
-        textElem.innerHTML = `Correct, the secret number is ${randomNumber}`;
-        
-    }
 }
